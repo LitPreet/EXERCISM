105f95b5ef3f210aa7c1793d9f59125cc79d09fe
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2022-12-08 22:19:42 +0530

    Create Resistor Color Duo.js

---
+++ Resistor Color Duo.js
@@ -0,0 +1,5 @@
+const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+export const decodedValue = colors => +colors
+  .slice(0, 2)
+  .map(color => COLORS.indexOf(color))
+  .join('');
