ab5c5a723181c4e2270d8484cfaf6edd5d90b044
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2022-12-11 07:48:06 +0530

    Create pangram.js

---
+++ pangram.js
@@ -0,0 +1,19 @@
+// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+export const isPangram = (words) => {
+  var unique = new Set();    
+  for(const w of words){
+    if((w >= 'a' && w <= 'z') || 
+       (w >= 'A' && w <= 'Z')){
+      unique.add(w.toLowerCase()); 
+    }
+  }
+  
+  if(unique.size === 26){
+    return true;
+  } else {
+    return false;
+  }
+  
+};
