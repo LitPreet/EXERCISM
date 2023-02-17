f198a588db70a8766e66dbff437ed0706705b009
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2023-02-17 12:41:14 +0530

    Update Resistor Color1.js

--- Resistor Color1.js
+++ Resistor Color1.js
@@ -1,4 +1,4 @@
-//Resistor Color1 js
+//Resistor Color1 js color code
 // This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
