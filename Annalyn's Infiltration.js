a616ecc44a74c3b72c00d8e5a45ca5bbe0dc8d7e
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2023-05-04 19:59:47 +0530

    Update Annalyn's Infiltration.js

--- Annalyn's Infiltration.js
+++ Annalyn's Infiltration.js
@@ -1,5 +1,5 @@
 // @ts-check
-//Annalyn's infiltration 
+//Annalyn's infiltration  js
 //
 // ☝🏽 The line above enables type checking for this file. Various IDEs interpret
 // the @ts-check directive. It will give you helpful autocompletion on the web
