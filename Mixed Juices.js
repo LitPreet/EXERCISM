2f4607d671fb3ed8850ccd85793884a63bff66d6
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2023-05-29 20:23:51 +0530

    Update Mixed Juices.js

--- Mixed Juices.js
+++ Mixed Juices.js
@@ -1,5 +1,5 @@
 // @ts-check
-//mixed juices js program 
+//mixed juices js 
 // The line above enables type checking for this file. Various IDEs interpret
 // the @ts-check directive. It will give you helpful autocompletion when
 // implementing this exercise.
