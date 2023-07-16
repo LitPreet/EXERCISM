63c27ba1cd56586f2c3bf69b9c1cd31f5ed70908
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2023-07-16 20:25:57 +0530

    Update Custom Signs.js

--- Custom Signs.js
+++ Custom Signs.js
@@ -1,5 +1,5 @@
 // @ts-check
-//custom sign 
+//custom sign je
 
 // The line above enables type checking for this file. Various IDEs interpret
 // the @ts-check directive. It will give you helpful autocompletion when
